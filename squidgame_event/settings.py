dd62db55f4a4b63db33fb10ef6dbf68bc69d10c5
tenetbeliefmsajce <tenetbelief.events@email.com>
2025-03-10 18:41:03 +0000

    Saved local changes before merging

--- squidgame_event/settings.py
+++ squidgame_event/settings.py
@@ -106,8 +106,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'tenetbelief.events@gmail.com'
+EMAIL_HOST_PASSWORD = 'peuu kaqw qvcv trer'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_SUBJECT_PREFIX = '[Squid Game Event] '
 
